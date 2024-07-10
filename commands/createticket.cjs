d0640f7bc05aeb6667b6235b16780a0ade7dd274
Ng, Ben <benjamin.b.ng@accenture.com>
2024-07-10 00:10:26 -0400

    console log

--- commands/createticket.cjs
+++ commands/createticket.cjs
@@ -2,6 +2,6 @@ module.exports = {
     name: "createticket",
     description: "Create a new support ticket",
     execute(message, args) {
-
+        console.log("Creating new support ticket");
     }
 }
