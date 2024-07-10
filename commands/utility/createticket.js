83ee5e21650e15f23d753a587485111c3d2acf0d
Ng, Ben <benjamin.b.ng@accenture.com>
2024-07-10 14:08:05 -0400

    ping2

---
+++ commands/utility/createticket.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ping2')
+        .setDescription('Replies with Pong!'),
+    async execute(interaction) {
+        await interaction.reply('Pong2!');
+    },
+};
