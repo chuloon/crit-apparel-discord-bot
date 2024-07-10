532c6d703ae9c7df5b742945350f6675d95ac205
Ng, Ben <benjamin.b.ng@accenture.com>
2024-07-10 11:47:04 -0400

    deploy

--- index.js
+++ index.js
@@ -12,21 +12,23 @@ const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
+    // Grab all the command files from the commands directory you created earlier
     const commandsPath = path.join(foldersPath, folder);
     const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
     for (const file of commandFiles) {
         const filePath = path.join(commandsPath, file);
         const command = require(filePath);
-        // Set a new item in the Collection with the key as the command name and the value as the exported module
         if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command);
+            commands.push(command.data.toJSON());
         } else {
             console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
         }
     }
 }
 
-const rest = new REST().setToken(process.env.BOT_TOKEN);
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(token);
 
 // and deploy your commands!
 (async () => {
