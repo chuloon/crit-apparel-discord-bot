8fc47622d95f8ddce851ca9e1adf45a3466cebfd
Ng, Ben <benjamin.b.ng@accenture.com>
2024-07-10 16:11:42 -0400

    vowel

--- commands/utility/createticket.js
+++ commands/utility/createticket.js
@@ -21,7 +21,7 @@ module.exports = {
     async execute(interaction) {
         // Fetch the #support-tickets channel. 1260666089407250516 is the channel id
         const channel = await interaction.guild.channels.fetch('1260666089407250516');
-        const dateTimeHex = Date.now().toString(16);
+        const dateTimeHex = Date.now().toString(16).toUpperCase();
         const thread = await channel.threads.create({
             name: dateTimeHex,
             autoArchiveDuration: ThreadAutoArchiveDuration.OneWeek,
@@ -33,6 +33,10 @@ module.exports = {
         // const modTeamRoleId = '1255930753016139838';
         const modTeamRoleId = null;
         const reason = interaction.options.getString('reason');
-        thread.send(`Hey there <@&${modTeamRoleId}>! <@${interaction.user.id}> needs some help with a \`${reason}\`. For future reference, your ticket number is \`#${dateTimeHex}\``)
+        thread.send(`Hey there <@&${modTeamRoleId}>! <@${interaction.user.id}> needs some help with a${isVowel(reason[0]) ? 'n' : ''} \`${reason}\`. For future reference, your ticket number is \`#${dateTimeHex}\``)
     },
 };
+
+function isVowel(letter) {
+    return ['a', 'e', 'i', 'o', 'u'].indexOf(letter.toLowerCase()) !== -1
+}
