b4c3d36ccc6d12d6c2a4776f346a9c7c3b34f0b9
Ng, Ben <benjamin.b.ng@accenture.com>
2024-07-10 11:13:19 -0400

    add

--- index.js
+++ index.js
@@ -1,44 +1,53 @@
 const fs = require('fs');
-const { Client, Collection, GatewayIntentBits, Events } = require('discord.js');
-const { prefix } = require('./config.json')
+const { Client, REST, GatewayIntentBits, Events } = require('discord.js');
+const path = require('node:path');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-// get command files
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commands = [];
+// Grab all the command folders from the commands directory you created earlier
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
-// parse command files into the client commands
-commandFiles.forEach(file => {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
-})
+for (const folder of commandFolders) {
+    // Grab all the command files from the commands directory you created earlier
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            commands.push(command.data.toJSON());
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
+const rest = new REST().setToken(token);
+
+// and deploy your commands!
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+        // The put method is used to fully refresh all commands in the guild with the current set
+        const data = await rest.put(
+            Routes.applicationGuildCommands(clientId, guildId),
+            { body: commands },
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (error) {
+        // And of course, make sure you catch and log any errors!
+        console.error(error);
+    }
+})();
 
 // bot is loaded and ready
 client.once(Events.ClientReady, readyClient => {
     console.log("Crit Apparel Bot online");
 });
-
-// code to run on message
-// client.on('messageCreate', message => {
-//     const args = message.content.slice(prefix.length).split(/ +/);
-//     const commandName = args.shift().toLowerCase();
-//     const command = client.commands.get(commandName);
-
-//     if (!client.commands.has(commandName)) return;
-
-//     if (message.channel.name === undefined) {
-//         message.reply("Your commands must be within the server, not through DMs.");
-//         return;
-//     }
-
-//     try {
-//         command.execute(message, args);
-//     }
-//     catch (e) {
-//         console.error(e);
-//         message.reply("There was an error executing that command. Please try again or alert the admin team.");
-//     }
-// })
 
 client.login(process.env.BOT_TOKEN);
