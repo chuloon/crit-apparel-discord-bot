e8a4071d6240507afee1d42cb593adfcf6f4e946
Ng, Ben <benjamin.b.ng@accenture.com>
2024-07-10 15:22:07 -0400

    choices

--- commands/utility/createticket.js
+++ commands/utility/createticket.js
@@ -3,7 +3,21 @@ const { SlashCommandBuilder, ThreadAutoArchiveDuration, ChannelType } = require(
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('createticket')
-        .setDescription('Creates a support ticket'),
+        .setDescription('Creates a support ticket')
+        .addStringOption(option =>
+            option
+                .setName('Reason')
+                .setDescription('What can we help you with?')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'Address Change', value: 'address_change' },
+                    { name: 'Order Status', value: 'order_status' },
+                    { name: 'Incorrect or Damaged Item', value: 'defective_item' },
+                    { name: 'Expedited Order Request', value: 'expedited_request' },
+                    { name: 'Product Design Request', value: 'product_design_request' },
+                    { name: 'General Questions', value: 'general_question' },
+                )
+        ),
     async execute(interaction) {
         // Fetch the #support-tickets channel. 1260666089407250516 is the channel id
         const channel = await interaction.guild.channels.fetch('1260666089407250516');
