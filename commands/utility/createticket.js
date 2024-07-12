da58ddea7ea298903b1592eacab3b64299fa058c
Ng, Ben <benjamin.b.ng@accenture.com>
2024-07-12 00:31:42 -0400

    test

--- commands/utility/createticket.js
+++ commands/utility/createticket.js
@@ -29,7 +29,7 @@ module.exports = {
         });
         // TODO Add mod team to private thread
         const modTeamRoleId = '1255930753016139838';
-        console.log(interaction.guild.members);
+        console.log(await interaction.guild.members.fetch().filter(member => member.roles.cache.has(modTeamRoleId)));
         await thread.members.add(interaction.user.id);
         await interaction.reply({ content: `A new ticket has been created. You can view it here <#${thread.id}>`, ephemeral: true })
 
