d7a8e094cd65ae041f2cdeed75061eb7c57ebdf3
Ng, Ben <benjamin.b.ng@accenture.com>
2024-07-26 15:49:06 -0400

    embed

--- commands/utility/embed.js
+++ commands/utility/embed.js
@@ -9,11 +9,11 @@ module.exports = {
         const embed = new EmbedBuilder()
             .setTitle("How to use Ticket Critter")
             .setDescription("Create support tickets and more!")
-            .addFields(
-                { name: "Basic Usage", value: "Using slash commands, you can create support tickets to get help and set your server nickname" },
-                { name: "Create a ticket", value: "Use the \`/createticket\` command to create a support ticket. Expected field: Reason for creating the ticket", inline: true },
-                { name: "Register yourself", value: "Use the \`/register\` command to set your name in the server so we know who you are. Expected fields: first/last name*, organization*, and gamer tag" }
-            )
+            // .addFields(
+            //     { name: "Basic Usage", value: "Using slash commands, you can create support tickets to get help and set your server nickname" },
+            //     { name: "Create a ticket", value: "Use the \`/createticket\` command to create a support ticket. Expected field: Reason for creating the ticket", inline: true },
+            //     { name: "Register yourself", value: "Use the \`/register\` command to set your name in the server so we know who you are. Expected fields: first/last name*, organization*, and gamer tag" }
+            // )
             .setImage("https://cdn.discordapp.com/attachments/1222641546634203186/1260665130945871903/Lime_Letter.png?ex=66a53d65&is=66a3ebe5&hm=189a83638f0c9bcc26583783f83cfd977e16999c524afebda55ba49839271aef&")
             .setFooter("To play your best, you have to wear the best");
 
