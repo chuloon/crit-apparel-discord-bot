eae82716514fdcd944eed3e57da2435efed53c24
Ng, Ben <benjamin.b.ng@accenture.com>
2024-07-26 16:15:06 -0400

    bot status

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, ActivityType } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildPresences] });
 
@@ -46,5 +46,13 @@ client.on(Events.InteractionCreate, async interaction => {
         }
     }
 });
+
+client.user.setPresence({
+    activities: [{
+        name: "Use the \`/createticket\` command to get started",
+        type: ActivityType.Listening
+    }],
+    status: "online"
+})
 
 client.login(process.env.BOT_TOKEN);
