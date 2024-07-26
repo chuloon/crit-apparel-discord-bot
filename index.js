3d1256501fac3f7f4badf366a176b97a31b81c7f
Ng, Ben <benjamin.b.ng@accenture.com>
2024-07-26 16:25:27 -0400

    update

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ for (const folder of commandFolders) {
 
 client.once(Events.ClientReady, readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-    client.user.setActivity("\`/createticket\` to get started", { type: ActivityType.Listening })
+    client.user.setActivity("Use /createticket to get started")
 });
 
 client.on(Events.InteractionCreate, async interaction => {
